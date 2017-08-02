434628ffbc8428e11c6e56c6f2b090b56e6ab3ed
@jotadeveloper <@jotadeveloper>
2017-08-02 07:30:04 +0200

    feat: add stream library

---
+++ core/streams/test/mystreams.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let ReadTarball = require('../src/index').ReadTarball;
+
+describe('mystreams', function() {
+  it('should delay events', function(cb) {
+    let test = new ReadTarball();
+    test.abort();
+    setTimeout(function() {
+      test.abort = function() {
+        cb();
+      };
+      test.abort = function() {
+        throw Error('fail');
+      };
+    }, 10);
+  });
+});
+
