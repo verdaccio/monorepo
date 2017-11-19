324729650ca805985b4f0b8d0e359435915fdb70
@jotadeveloper <@jotadeveloper>
2017-11-19 09:21:22 +0100

    refactor: add typings for local-storage

--- core/types/src/types.js
+++ core/types/src/types.js
@@ -1,10 +1,187 @@
 /* @flow */
 
+declare type Callback = Function;
+
+declare type StorageList = Array<string>;
+
+declare type LocalStorage = {
+	list: StorageList;
+	secret: string;
+}
+
+declare type Dist = {
+	integrity?: string;
+	shasum: string;
+	tarball: string;
+}
+
+declare type Author = {
+	name: string;
+	email: string;
+}
+
+declare type Version = {
+	name: string;
+	version: string;
+	devDependencies: string;
+	directories: any;
+	dist: Dist;
+	author: string;
+	homemage: string;
+	license: string;
+	readme: string;
+	readmeFileName: string;
+	description: string;
+	bin: string;
+	bugs: any;
+	repository: string;
+	homepage: string;
+	dist: Dist;
+	contributors: Array<string>;
+	keywords: string | Array<string>;
+	_npmUser: Author;
+	_hasShrinkwrap: boolean;
+};
+
+declare type Logger = {
+	child: (conf: any) => any;
+	debug: (conf: any, template?: mixed) => void;
+	error: (conf: any, template?: mixed) => void;
+	http: (conf: any, template?: mixed) => void;
+	trace: (conf: any, template?: mixed) => void;
+	info: (conf: any, template?: mixed) => void;
+}
+
+declare type Versions = {
+	[key: string]: Version;
+}
+
+declare type DistFile = {
+	url: string;
+	sha: string;
+	registry?: string;
+}
+
+	declare type MergeTags = {
+	[key: string]: string;
+}
+
+declare type DistFiles = {
+	[key: string]: DistFile;
+}
+
+declare type AttachMents = {
+	[key: string]: Version;
+}
+
+declare type GenericBody = {
+	[key: string]: string;
+}
+
+declare type UpLinks = {
+	[key: string]: Version;
+}
+
+declare type Tags = {
+	[key: string]: Version;
+}
+
+declare type Package = {
+	_id?: string;
+	name: string;
+	versions: Versions;
+	'dist-tags': GenericBody;
+	time: GenericBody;
+	readme?: string;
+	_distfiles: DistFiles;
+	_attachments: AttachMents;
+	_uplinks: UpLinks;
+	_rev?: string;
+}
+
+declare interface ILocalStorage {
+	add(name: string): void;
+	remove(name: string): void;
+	get(): StorageList;
+	sync(): void;
+}
+
+declare type UpLinkConf = {
+	storage: string;
+	url: string;
+}
+
+declare type PackageAccess = {
+	storage: string;
+	publish: string;
+	proxy: string;
+	access: string;
+}
+
+declare type PackageList = {
+	[key: string]: PackageAccess;
+}
+
+declare type UpLinksConfList = {
+	[key: string]: UpLinkConf;
+}
+
+declare type Config = {
+	storage: string;
+	packages: PackageList;
+	uplinks: UpLinksConfList;
+	self_path: string;
+	checkSecretKey: (token: string) => string;
+	getMatchedPackagesSpec: (storage: string) => PackageAccess;
+}
+
+declare type SyncReturn = Error | void;
+
+declare module "@verdaccio/local-storage" {
+	declare export interface ILocalFS {
+		createWriteStream(name: string): Stream;
+		createReadStream(readTarballStream: any, callback?: Function): Stream;
+		readJSON(fileName: string, callback: Callback): void;
+		createJSON(name: string, value: any, cb: Function): void;
+		deleteJSON(fileName: string, callback: Callback): void;
+		unlockJSON(fileName: string, callback: Callback): void;
+		removePackage(callback: Callback): void;
+		lockAndReadJSON(fileName: string, callback: Callback): void;
+		writeJSON(fileName: string, json: Package, callback: Callback): void;
+	}
+
+	declare export interface ILocalData {
+		add(name: string): SyncReturn;
+		remove(name: string): SyncReturn;
+		get(): StorageList;
+		getPackageStorage(packageInfo: string, packagePath: string): ILocalFS;
+		sync(): ?SyncReturn;
+	}
+
+	declare class LocalDatabase<ILocalData>{
+		constructor(config: Config, logger: Logger): ILocalData;
+	}
+
+	declare module.exports: typeof LocalDatabase;
+}
+
+
 declare module "@verdaccio/types" {
 
 	declare export type Stdout = stream$Writable | tty$WriteStream;
 	declare export type Stdin = stream$Readable | tty$ReadStream;
-	declare export type SyncReturn = Error | void;
+	declare export type Package = Package;
+	declare export type MergeTags = MergeTags;
+	declare export type Version = Version;
+	declare export type Callback = Callback;
+	declare export type Logger = Logger;
+	declare export type DistFile = DistFile;
+	declare export type Config = Config;
+	declare export type PackageList = PackageList;
+	declare export type UpLinksConfList = UpLinksConfList;
+	declare export type ILocalStorage = ILocalStorage;
+	declare export type UpLinkConf = UpLinkConf;
+	declare export type PackageAccess = PackageAccess;
 
 	declare export interface IStorage {
 		addPackage(name: string, info: Package, callback: Callback): void;
@@ -36,169 +213,14 @@ declare module "@verdaccio/types" {
 		writeJSON(fileName: string, json: Package, callback: Callback): void;
 	}
 
-	declare export interface ILocalFS {
-		createWriteStream(name: string): Stream;
-		createReadStream(readTarballStream: any, callback?: Function): Stream;
-		readJSON(fileName: string, callback: Callback): void;
-		createJSON(name: string, value: any, cb: Function): void;
-		deleteJSON(fileName: string, callback: Callback): void;
-		unlockJSON(fileName: string, callback: Callback): void;
-		removePackage(callback: Callback): void;
-		lockAndReadJSON(fileName: string, callback: Callback): void;
-		writeJSON(fileName: string, json: Package, callback: Callback): void;
-	}
-
-	declare export interface ILocalData {
-		add(name: string): SyncReturn;
-		remove(name: string): SyncReturn;
-		get(): StorageList;
-		getPackageStorage(packageInfo: string, packagePath: string): ILocalFS;
-		sync(): ?SyncReturn;
-	}
-
-	declare export interface ILocalStorage {
-		add(name: string): void;
-		remove(name: string): void;
-		get(): StorageList;
-		sync(): void;
-	}
-
-	declare export type UpLinkConf = {
-		storage: string;
-		url: string;
-	}
-
-	declare export type PackageAccess = {
-		storage: string;
-		publish: string;
-		proxy: string;
-		access: string;
-	}
-
-	declare export type PackageList = {
-		[key: string]: PackageAccess;
-	}
-
-	declare export type UpLinksConfList = {
-		[key: string]: UpLinkConf;
-	}
-
-	declare export type Config = {
-		storage: string;
-		packages: PackageList;
-		uplinks: UpLinksConfList;
-		self_path: string;
-		checkSecretKey: (token: string) => string;
-		getMatchedPackagesSpec: (storage: string) => PackageAccess;
-	}
-
-	declare export type Callback = Function;
-
-	declare export type StorageList = Array<string>;
-
-	declare export type LocalStorage = {
-		list: StorageList;
-		secret: string;
-	}
-
-	declare export type Utils = {
-			ErrorCode: any;
-			getLatestVersion: Callback;
-			is_object: (value: any) => boolean;
-			validate_name: (value: any) => boolean;
-			tag_version: (value: any, version: string, tag: string) => void;
-			normalize_dist_tags: (pkg: Package) => void;
-			semver_sort: (keys: Array<string>) => Array<string>;
-	}
-
-	declare export type Dist = {
-		integrity?: string;
-		shasum: string;
-		tarball: string;
-	}
-
-	declare export type Author = {
-		name: string;
-		email: string;
-	}
-
-	declare export type Version = {
-		name: string;
-		version: string;
-		devDependencies: string;
-		directories: any;
-		dist: Dist;
-		author: string;
-		homemage: string;
-		license: string;
-		readme: string;
-		readmeFileName: string;
-		description: string;
-		bin: string;
-		bugs: any;
-		repository: string;
-		homepage: string;
-		dist: Dist;
-		contributors: Array<string>;
-		keywords: string | Array<string>;
-		_npmUser: Author;
-		_hasShrinkwrap: boolean;
-	};
-
-	declare export type Logger = {
-		child: (conf: any) => any;
-		debug: (conf: any, template?: mixed) => void;
-		error: (conf: any, template?: mixed) => void;
-		http: (conf: any, template?: mixed) => void;
-		trace: (conf: any, template?: mixed) => void;
-		info: (conf: any, template?: mixed) => void;
-	}
-
-	declare export type Versions = {
-		[key: string]: Version;
-	}
-
-	declare export type DistFile = {
-		url: string;
-		sha: string;
-		registry?: string;
-	}
-
-		declare export  type MergeTags = {
-		[key: string]: strings;
-	}
-
-	declare export  type DistFiles = {
-		[key: string]: DistFile;
-	}
-
-	declare export type AttachMents = {
-		[key: string]: Version;
-	}
-
-	declare export type GenericBody = {
-		[key: string]: string;
-	}
-
-	declare export type UpLinks = {
-		[key: string]: Version;
-	}
-
-	declare export type Tags = {
-		[key: string]: Version;
-	}
-
-	declare export type Package = {
-		_id?: string;
-		name: string;
-		versions: Versions;
-		'dist-tags': GenericBody;
-		time: GenericBody;
-		readme?: string;
-		_distfiles: DistFiles;
-		_attachments: AttachMents;
-		_uplinks: UpLinks;
-		_rev?: string;
+	declare type Utils = {
+		ErrorCode: any;
+		getLatestVersion: Callback;
+		is_object: (value: any) => boolean;
+		validate_name: (value: any) => boolean;
+		tag_version: (value: any, version: string, tag: string) => void;
+		normalize_dist_tags: (pkg: Package) => void;
+		semver_sort: (keys: Array<string>) => Array<string>;
 	}
 
 }
