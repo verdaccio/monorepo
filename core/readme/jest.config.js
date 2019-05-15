f4bbf3af09deaaea775a82b5205bc453f73704e9
@jotadeveloper <@jotadeveloper>
2019-05-15 07:59:29 +0200

    feat(readme): import readme package
    - chore: innitial commit
    
    - chore: update circleci
    
    - chore: add eslint
    
    - test: add basic test
    
    - chore: fix eslint
    
    - chore: improve linting
    
    - chore: update husky
    
    - test: add image test
    
    - test: add xss scenarios
    
    - test: add xss scenarios
    
    - chore: add codecov
    
    - chore: add more scenarios
    
    - chore: add license
    
    - chore: add access public
    
    - chore: add standard release
    
    - chore(release): 1.0.1
    
    - fix(build): remove publish script
    
    - chore(release): 1.0.2
    
    - fix(build): lib folder as main
    
    - chore(release): 1.0.3
    
    - chore: add more scenarios
    
    - fix: update dependencies
    
    - chore(release): 1.0.4
    
    - chore(deps): bump lodash.template from 4.4.0 to 4.5.0
    
      Bumps [lodash.template](https://github.com/lodash/lodash) from 4.4.0 to 4.5.0.
      - [Release notes](https://github.com/lodash/lodash/releases)
      - [Commits](https://github.com/lodash/lodash/compare/4.4.0...4.5.0)
    
      Signed-off-by: dependabot[bot] <support@github.com>

---
+++ core/readme/jest.config.js
@@ -0,0 +1,12 @@
+/* eslint comma-dangle: 0 */
+
+module.exports = {
+  name: 'readme-jest',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
+  verbose: true,
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['node_modules', 'fixtures']
+};
