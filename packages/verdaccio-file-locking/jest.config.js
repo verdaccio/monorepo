11e3d487c9c84b2166bebb34a61e06572fd8a4a3
Sergio Herrera Guzmán <sergio@sergiohgz.eu>
2019-07-17 14:40:20 +0200

    chore: recreate project architecture
    - Remove old packages and configurations contained to add new in the future
    - Upgrade root dependencies
    - Improve project structure
    - Modernize CI with new arch and scripts

--- packages/verdaccio-file-locking/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'file-locking',
-    verbose: true,
-    collectCoverage: true,
-    coveragePathIgnorePatterns: ['node_modules', '_storage', 'fixtures', 'lib'],
-    testURL: 'http://localhost',
-};
