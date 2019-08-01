74a53acc5232e9451e7d7b82548c02934a45e934
Sergio Herrera Guzmán <sergio@sergiohgz.eu>
2019-08-01 20:16:32 +0200

    refactor(eslint-config): complete Prettier settings

--- tools/eslint-config/index.js
+++ tools/eslint-config/index.js
@@ -1,24 +1,19 @@
 module.exports = {
-  extends: [
-    './rules/base',
-    './rules/typescript',
-    './rules/import',
-    './rules/prettier'
-  ].map(require.resolve),
+  extends: ['./rules/base', './rules/typescript', './rules/import', './rules/prettier'].map(require.resolve),
   env: {
     es6: true,
     node: true,
-    jest: true
+    jest: true,
   },
   globals: {
-    __APP_VERSION__: true
+    __APP_VERSION__: true,
   },
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2019,
     ecmaFeatures: {
       impliedStrict: true,
-      jsx: true
-    }
-  }
+      jsx: true,
+    },
+  },
 };
