4049f2089ff476664f3cf89daf01dae081f40ff1
Sergio Herrera Guzmán <sergio@sergiohgz.eu>
2019-08-02 09:30:35 +0200

    chore(babel-preset): fix ESLint config
    - Remove unnecessary ESLint rules
    - Use new Prettier settings

--- tools/babel-preset/lib/builder.js
+++ tools/babel-preset/lib/builder.js
@@ -1,34 +1,25 @@
-const MIN_NODE_VERSION = "8.15";
-const DOCKER_NODE_VERSION = "10";
+const MIN_NODE_VERSION = '8.15';
+const DOCKER_NODE_VERSION = '10';
 const BROWSER_TARGETS = {
-  browsers: [
-    "last 5 versions",
-    "FireFox >= 44",
-    "Safari >= 7",
-    "Explorer 11",
-    "last 4 Edge versions"
-  ]
+  browsers: ['last 5 versions', 'FireFox >= 44', 'Safari >= 7', 'Explorer 11', 'last 4 Edge versions'],
 };
 
 const defaultPresets = (node = MIN_NODE_VERSION) => [
   [
-    "@babel/env",
+    '@babel/env',
     {
-      targets: { node }
-    }
-  ]
+      targets: { node },
+    },
+  ],
 ];
 
-const reactPreset = ["@babel/react"];
+const reactPreset = ['@babel/react'];
 
-const defaultPlugins = [
-  "@babel/proposal-class-properties",
-  "@babel/proposal-object-rest-spread"
-];
+const defaultPlugins = ['@babel/proposal-class-properties', '@babel/proposal-object-rest-spread'];
 
 const typescriptRequirements = {
-  presets: ["@babel/typescript"],
-  plugins: []
+  presets: ['@babel/typescript'],
+  plugins: [],
 };
 
 const getTranspiler = () => {
@@ -38,48 +29,41 @@ const getTranspiler = () => {
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 module.exports = (api, opts, env, isPro) => {
   // see docs about api at https://babeljs.io/docs/en/config-files#apicache
-  api.assertVersion("^7.0.0");
+  api.assertVersion('^7.0.0');
   const minNodeVersion = opts.node || MIN_NODE_VERSION;
   if (opts.debug) {
     /* eslint no-console: 0 */
-    console.log("minNodeVersion:", minNodeVersion);
+    console.log('minNodeVersion:', minNodeVersion);
   }
   const transpiler = getTranspiler();
 
-  if (env === "ui") {
+  if (env === 'ui') {
     return {
-      presets: [["@babel/env", { targets: BROWSER_TARGETS }]]
-        .concat(reactPreset)
-        .concat(transpiler.presets),
+      presets: [['@babel/env', { targets: BROWSER_TARGETS }]].concat(reactPreset).concat(transpiler.presets),
       plugins: defaultPlugins.concat([
-        "react-hot-loader/babel",
-        "@babel/transform-runtime",
-        "@babel/syntax-dynamic-import",
-        "emotion"
-      ])
+        'react-hot-loader/babel',
+        '@babel/transform-runtime',
+        '@babel/syntax-dynamic-import',
+        'emotion',
+      ]),
     };
-  } else if (env === "test") {
+  } else if (env === 'test') {
     return {
       presets: defaultPresets(minNodeVersion)
         .concat(reactPreset)
         .concat(transpiler.presets),
-      plugins: defaultPlugins.concat([
-        "babel-plugin-dynamic-import-node",
-        "emotion"
-      ])
+      plugins: defaultPlugins.concat(['babel-plugin-dynamic-import-node', 'emotion']),
     };
-  } else if (env === "docker") {
+  } else if (env === 'docker') {
     return {
-      presets: [
-        ["@babel/env", { targets: { node: DOCKER_NODE_VERSION } }]
-      ].concat(transpiler.presets),
-      plugins: defaultPlugins.concat(transpiler.plugins)
+      presets: [['@babel/env', { targets: { node: DOCKER_NODE_VERSION } }]].concat(transpiler.presets),
+      plugins: defaultPlugins.concat(transpiler.plugins),
     };
   } else {
     // registry or something else
     return {
       presets: defaultPresets(minNodeVersion).concat(transpiler.presets),
-      plugins: defaultPlugins.concat(transpiler.plugins)
+      plugins: defaultPlugins.concat(transpiler.plugins),
     };
   }
 };
