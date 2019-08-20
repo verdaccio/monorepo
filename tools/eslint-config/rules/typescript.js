acf83d51a1caaa8edf8e5ce789d824f5b192bc0b
Sergio Herrera Guzmán <sergio@sergiohgz.eu>
2019-08-21 00:50:30 +0200

    refactor(eslint-config): temporal set to warn some rules

--- tools/eslint-config/rules/typescript.js
+++ tools/eslint-config/rules/typescript.js
@@ -4,5 +4,10 @@ module.exports = {
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/indent': ['error', 2],
     '@typescript-eslint/explicit-member-accessibility': ['warn'],
+    // TODO temporal fix
+    '@typescript-eslint/no-inferrable-types': 'warn',
+    '@typescript-eslint/ban-ts-ignore': 'warn',
+    '@typescript-eslint/no-empty-function': 'warn',
+    '@typescript-eslint/no-this-alias': 'warn',
   },
 };
