5c9e9e001181fc668b34dc118e53ca7998e9d8f9
Szilard Nagy <nszilard94@gmail.com>
2020-01-24 15:56:39 +0000

    refactor(verdaccio-aws-s3-storage): extracting config value setter into a separate function

--- plugins/aws-s3-storage/src/index.ts
+++ plugins/aws-s3-storage/src/index.ts
@@ -15,6 +15,7 @@ import { S3Config } from './config';
 import S3PackageManager from './s3PackageManager';
 import { convertS3Error, is404Error } from './s3Errors';
 import addTrailingSlash from './addTrailingSlash';
+import setConfigValue from './setConfigValue';
 
 export default class S3Database implements IPluginStorage<S3Config> {
   public logger: Logger;
@@ -33,8 +34,14 @@ export default class S3Database implements IPluginStorage<S3Config> {
     if (!this.config.bucket) {
       throw new Error('s3 storage requires a bucket');
     }
-    this.config.bucket = process.env[this.config.bucket] || this.config.bucket;
-    const configKeyPrefix = process.env[this.config.keyPrefix] || this.config.keyPrefix;
+
+    this.config.bucket = setConfigValue(this.config.bucket);
+    this.config.keyPrefix = setConfigValue(this.config.keyPrefix);
+    this.config.region = setConfigValue(this.config.region);
+    this.config.accessKeyId = setConfigValue(this.config.accessKeyId);
+    this.config.secretAccessKey = setConfigValue(this.config.secretAccessKey);
+
+    const configKeyPrefix = this.config.keyPrefix;
     this._localData = null;
     this.config.keyPrefix = addTrailingSlash(configKeyPrefix);
 
@@ -42,10 +49,10 @@ export default class S3Database implements IPluginStorage<S3Config> {
 
     this.s3 = new S3({
       endpoint: this.config.endpoint,
-      region: process.env[this.config.region!] || this.config.region,
+      region: this.config.region,
       s3ForcePathStyle: this.config.s3ForcePathStyle,
-      accessKeyId: process.env[this.config.accessKeyId!] || this.config.accessKeyId,
-      secretAccessKey: process.env[this.config.secretAccessKey!] || this.config.secretAccessKey,
+      accessKeyId: this.config.accessKeyId,
+      secretAccessKey: this.config.secretAccessKey,
     });
   }
 
