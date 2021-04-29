a504875c474bed4ce026894de8394c86ed567432
Juan Picado <juanpicado19@gmail.com>
2021-04-29 22:06:12 +0200

    chore: migrate to pnpm

--- tools/testing-utilities/test/package-generator.spec.ts
+++
@@ -1,9 +0,0 @@
-import { generatePackageBody } from '../src/index';
-
-describe('package generator', () => {
-  test('should return a basic body', () => {
-    const metadata = generatePackageBody('test', ['1.0.0']);
-
-    expect(metadata).toBeDefined();
-  });
-});
